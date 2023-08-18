d9af4ac4309671ff9cf869d7780bb9228dee22ed
fbehddn <tom0611@naver.com>
2023-08-18 22:44:08 +0900

    로그인 기능 추가

--- src/main/java/hello/login/domain/member/Member.java
+++ src/main/java/hello/login/domain/member/Member.java
@@ -7,10 +7,10 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class Member {
 
-    private Long id;
+    private Long id; //db 저장 및 관리
 
     @NotEmpty
-    private String loginId; //로그인 ID
+    private String loginId; //로그인 ID (사용자 직접 사용)
     @NotEmpty
     private String name;
     @NotEmpty
--- src/main/java/hello/login/web/login/LoginForm.java
+++ src/main/java/hello/login/web/login/LoginForm.java
@@ -1,7 +1,11 @@
 package hello.login.web.login;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotEmpty;
 
+@Getter @Setter
 public class LoginForm {
 
     @NotEmpty
