00177ca3cc21843c1edfb42d722ab70667b0c932
fbehddn <tom0611@naver.com>
2023-08-19 17:12:52 +0900

    세션 조회 (리팩토링 전)

--- src/main/java/hello/login/web/session/SessionManager.java
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -36,6 +37,33 @@ public class SessionManager {
         response.addCookie(mySessionCookie);
     }
 
+    /**
+     * 세션 조회
+     */
+    public Object getSession(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
 
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(SESSION_COOKIE_NAME)) {
+                return sessionStore.get(cookie.getValue());
+            }
+        }
+        return null;
+    }
+
+    public Cookie findCoockie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
+
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
 }
 
